ceca9a58d3f8a142a92ffb75ff39c064ad1b7838
Philip Winnington <ftcpjw@yahoo.com>
2019-02-20 21:59:48 -0500

    added template for login and register

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0c38bfec65e63d30f16f21e391a4aabd'
@@ -27,14 +27,23 @@ def home():
 def about():
     return  render_template('about.html', title="About")
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You Have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='login', form=form)
 
 if __name__ == "__main__":
